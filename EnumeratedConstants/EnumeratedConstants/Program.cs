419171f4ff6eab57318d799ae8e5bd588f327e69
Andre Murrell <andre.murrell@outlook.com>
2017-07-11 10:15:06 -0400

    Add files via upload

---
+++ EnumeratedConstants/EnumeratedConstants/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumeratedConstants
+{
+    class Program
+    {
+        enum Color
+        {
+            Red,
+            Blue,
+            White, 
+        }
+
+
+        static void Main(string[] args)
+        {
+            Color favorite = Color.White;
+            switch(favorite)
+            {
+                case Color.Blue:
+                    Console.WriteLine("Blue is your favorite color silly!");
+                    break; 
+            }
+        }
+    }
+}
